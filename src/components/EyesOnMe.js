74fe95b47d2b8b0ab37bdc1d942965aa51a6a19a
Zaid Almuflahi <zaid.almuflahi1217@gmail.com>
2022-11-09 12:32:42 -0500

    commit tree 37e29816bc180830e709ac3b6f7a0cf09a221571

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe(){
+    const handleBlur = () =>{
+        console.log('Hey! Eyes on me!')
+    }
+    const handleFocus = () =>{
+        console.log('Good!')
+    }
+    return (
+       <button 
+       onBlur={handleBlur}
+       onFocus={handleFocus}
+       >Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,22 @@
 // Code Keypad Component Here
+import React from 'react';
 
 function Keypad (){
-    return (
-        <div></div>
-    )
-}
-
+    function handleChange(e) {
+       
+        console.log("Entering password...");
+        // console.log(e.target);
+      }
+    
+      return (
+        <div>
+          <input
+            type="password"
+            name="pin"
+            onChange={handleChange}
+            placeholder="Enter password..."
+          />
+        </div>
+      );
+    }
 export default Keypad;
